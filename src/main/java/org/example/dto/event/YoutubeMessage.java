ed9a7080fc76384d3310b984df1a0d292592fd4c
H8M33 <andyos194@gmail.com>
2023-02-28 00:14:54 +0300

    2

---
+++ src/main/java/org/example/dto/event/YoutubeMessage.java
@@ -0,0 +1,13 @@
+package org.example.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class YoutubeMessage {
+}
