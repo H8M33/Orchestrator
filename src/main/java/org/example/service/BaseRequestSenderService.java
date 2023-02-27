53919489d53faceedd11a60ac4947a17a207e5e5
H8M33 <andyos194@gmail.com>
2023-02-28 00:28:44 +0300

    4

--- src/main/java/org/example/service/BaseRequestSenderService.java
+++ src/main/java/org/example/service/BaseRequestSenderService.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +14,7 @@ public class BaseRequestSenderService implements RequestSenderService{
 
     private final RabbitTemplate template;
     private final MyRabbitProperties properties;
-    private Instant instant;
+    private Instant instant = Instant.from(LocalDateTime.of(2022,1,1,0,0,0));
 
     @Override
     public void sendRequest() {
