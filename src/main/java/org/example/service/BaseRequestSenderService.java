69543913925becc4fc9d6abac6ce45781bd3d300
H8M33 <andyos194@gmail.com>
2023-02-28 00:34:30 +0300

    4

--- src/main/java/org/example/service/BaseRequestSenderService.java
+++ src/main/java/org/example/service/BaseRequestSenderService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +16,8 @@ public class BaseRequestSenderService implements RequestSenderService{
 
     private final RabbitTemplate template;
     private final MyRabbitProperties properties;
-    private Instant instant = Instant.from(LocalDateTime.of(2022,1,1,0,0,0));
+
+    private Instant instant = LocalDateTime.of(2022,1,1,0,0,0).toInstant(ZoneOffset.UTC);
 
     @Override
     public void sendRequest() {
