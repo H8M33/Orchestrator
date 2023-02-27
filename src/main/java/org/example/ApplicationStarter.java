4361eda7bb18f3678baa897f331630634ce18480
H8M33 <andyos194@gmail.com>
2023-02-27 23:51:40 +0300

    Initial commit

---
+++ src/main/java/org/example/ApplicationStarter.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.config.property.MyRabbitProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(MyRabbitProperties.class)
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/config/property/MyRabbitProperties.java
@@ -0,0 +1,28 @@
+package org.example.config.property;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "service-rabbit")
+@Validated
+@Data
+public class MyRabbitProperties {
+    @NotNull
+    private String exchangeReceiverName;
+
+    @NotNull
+    private String exchangeSenderName;
+
+    @NotNull
+    private String telegramRoutingKey;
+
+    @NotNull
+    private String vkRoutingKey;
+
+    @NotNull
+    private String youtubeRoutingKey;
+
+}
+
