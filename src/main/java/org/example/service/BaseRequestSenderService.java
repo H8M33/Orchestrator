d2726e18a82a72af387f1d4759ca27957a382105
H8M33 <andyos194@gmail.com>
2023-02-28 17:32:50 +0300

    4

--- src/main/java/org/example/service/BaseRequestSenderService.java
+++ src/main/java/org/example/service/BaseRequestSenderService.java
@@ -1,8 +1,11 @@
 package org.example.service;
 
+import ch.qos.logback.core.util.FixedDelay;
 import lombok.RequiredArgsConstructor;
 import org.example.config.property.MyRabbitProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -11,6 +14,7 @@ import java.time.ZoneOffset;
 
 @Service
 @RequiredArgsConstructor
+@EnableScheduling
 public class BaseRequestSenderService implements RequestSenderService{
 
     private final RabbitTemplate template;
@@ -18,6 +22,7 @@ public class BaseRequestSenderService implements RequestSenderService{
 
     private Instant instant = LocalDateTime.of(2022,1,1,0,0,0).toInstant(ZoneOffset.UTC);
     @Override
+    @Scheduled(fixedDelay = 3600000)
     public void sendRequest() {
         template.convertAndSend(
                 properties.getExchangeSenderName(),
